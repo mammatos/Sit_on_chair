c84766a3ab5991e58aa52f23f8f8cb5d676c5257
mammatos <mariannawaranska@gmail.com>
2017-04-21 14:45:06 +0200

    add comments to calculatorSection

--- Kalkulator - dodatkowa funkcjonalność (dla chętnych)/js/app.js
+++ Kalkulator - dodatkowa funkcjonalność (dla chętnych)/js/app.js
@@ -7,20 +7,36 @@ for(var i = 0; i < calculatorListArrows.length; i++) {
         this.nextElementSibling.classList.toggle('visible');
     });
 }
-//This loop make list_panel visible after click on arrow.
+//This loop make 'list_panel' visible after click on arrow.
 
 var listPanels = document.getElementsByClassName('list_panel');
 var filler = document.getElementById('filler');
+var rubricColor = document.getElementById('rubricColor');
+var rubricPattern = document.getElementById('patternColor');
 
 for(var i = 0; i < listPanels.length; i++) {
+    //This loop iterates by every 'list_panel'.
     var itemsListPanels = listPanels[i].children;
-
+    
     for(var j = 0; j < itemsListPanels.length; j++) {
+        //This loop iterates by every 'item' of 'list_panel'.
         itemsListPanels[j].addEventListener('click', function() {
             console.log('click');
-            filler.style.display = "none";
+            
             var cloneItem = this;
-            // When client want to add item to summary_panel, 'Twój fotel' disappears by display:none.
+            //This variable stores last clicked item.
+
+            var newInnerContent = cloneItem.innerText;
+            
+            if (listPanels[0]) {
+                console.log("jestem nazwą krzesła");
+                filler.innerText = newInnerContent;
+            }
+            if (listPanels[1]) {
+                console.log("jestem kolorem krzesła");
+                rubricColor.innerText = newInnerContent;
+            }
+            //This code add 'item' which were clicked to 'filler' and 'rubricColor'. It doesn't quite work as I want because I want 'item' in one of them. I save this for example.
         });
     }
 }
